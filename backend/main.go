d63f5d8a6299677e9d0b45d7aec01365a2912f35
Conor <git@dbuidl.com>
2022-11-08 15:52:20 +0000

    Add initial api methods

--- backend/main.go
+++ backend/main.go
@@ -1,13 +1,84 @@
 package main
 
 import (
+	"encoding/json"
 	"log"
+	"net/http"
 
+	"github.com/labstack/echo/v5"
+	"github.com/pocketbase/dbx"
 	"github.com/pocketbase/pocketbase"
+	"github.com/pocketbase/pocketbase/tools/types"
+	"github.com/pocketbase/pocketbase/core"
 )
+
+type JsonResponse struct {
+	Success bool            `json:"success"`
+	Message string          `json:"message"`
+	Data    json.RawMessage `json:"data"`
+}
+
+type BaseModel struct {
+	Id string `db:"id"`
+    Created types.DateTime
+	Updated types.DateTime
+}
+
+type ApiKeyModel struct {
+	BaseModel
+}
 
 func main() {
 	app := pocketbase.New()
+
+	// this is the public config api
+
+	app.OnBeforeServe().Add(func(e *core.ServeEvent) error {
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodGet,
+			Path:   "/api/v1/get_config",
+			Handler: func(c echo.Context) error {
+				// get key from auth header
+				key := c.Request().Header.Get("Authorization")
+				if key == "" {
+					return c.JSON(http.StatusUnauthorized, JsonResponse{
+						Success: false,
+						Message: "missing authorization header",
+						Data:    nil,
+					})
+				}
+
+				// select all from api_key table
+				q := app.DB().Select("*").From("api_key").Where(dbx.HashExp{"key": key}).One()
+
+				return c.JSON(http.StatusOK, JsonResponse{
+					Success: true,
+					Message: "Success",
+					Data:    nil,
+				})
+			},
+			Middlewares: []echo.MiddlewareFunc{
+				//apis.RequireAdminOrUserAuth(),
+			},
+		})
+
+		e.Router.AddRoute(echo.Route{
+			Method: http.MethodOptions,
+			Path:   "/api/v1/get_config",
+			Handler: func(c echo.Context) error {
+				// send cors headers
+				c.Response().Header().Set("Access-Control-Allow-Origin", "*")
+				c.Response().Header().Set("Access-Control-Allow-Methods", "GET")
+				c.Response().Header().Set("Access-Control-Allow-Headers", "Content-Type, Authorization")
+				return c.NoContent(http.StatusOK)
+			},
+			Middlewares: []echo.MiddlewareFunc{
+				//apis.RequireAdminOrUserAuth(),
+			},
+		})
+
+		return nil
+	})
 
 	if err := app.Start(); err != nil {
 		log.Fatal(err)
